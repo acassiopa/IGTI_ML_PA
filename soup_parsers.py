dae0f171554e1c073b43ffe2c5872becce213fc2
acassiopa <alisson.cac@gmail.com>
2021-05-18 21:44:34 -0300

    Parsers' bugs fixed

--- soup_parsers.py
+++ soup_parsers.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 from bs4 import BeautifulSoup
-import dateutil.parser
+from dateutil import parser
 from pandas import DataFrame
 from urllib.request import Request, urlopen
 
@@ -23,7 +23,7 @@ class SoupParser(ABC):
         news_data['source']     = self.news_source
         news_data['url']        = url
         news_data['title']      = title
-        news_data['datetime']   = dateutil.parser.parse(date, ignoretz=True)
+        news_data['datetime']   = date
         news_data['raw_text']   = text
         self.news_array.append(news_data)
 
@@ -35,7 +35,7 @@ class InfoMoneyParser(SoupParser):
     news_source = 'InfoMoney'
 
     def parse(self) -> None:
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -43,14 +43,12 @@ class InfoMoneyParser(SoupParser):
             soup = BeautifulSoup(page, "html.parser")
 
             try:
-                time_tag = soup.find('time')
-                article_date = time_tag['datetime']
+                article_date = soup.find('time')['datetime']
+                article_date= parser.parse(article_date, ignoretz=True)
                 article_tag = soup.find(class_='article-content')
-                p_list = article_tag.find_all('p', class_=False)
-                for p in p_list:
-                    for tag in p.find_all():
-                        if tag.name not in valid_tags:
-                            tag.decompose()
+                for tag in article_tag.find_all():
+                    if tag.name not in valid_tags:
+                        tag.decompose()
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
@@ -65,7 +63,8 @@ class InvestingParser(SoupParser):
     news_source = 'Investing'
 
     def parse(self):
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
+        parserinfo = parser.parserinfo(dayfirst=True)
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -74,12 +73,11 @@ class InvestingParser(SoupParser):
 
             try:
                 article_date = soup.find(class_='contentSectionDetails').span.get_text()
+                article_date= parser.parse(article_date, parserinfo=parserinfo, ignoretz=True)
                 article_tag = soup.find(class_='articlePage')
-                p_list = article_tag.find_all('p', class_=False)
-                for p in p_list:
-                    for tag in p.find_all():
-                        if tag.name not in valid_tags:
-                            tag.decompose()
+                for tag in article_tag.find_all():
+                    if tag.name not in valid_tags:
+                        tag.decompose()
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
@@ -95,7 +93,8 @@ class MoneyTimesParser(SoupParser):
     news_source = 'MoneyTimes'
 
     def parse(self):
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
+        parserinfo = parser.parserinfo(dayfirst=True)
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -104,12 +103,11 @@ class MoneyTimesParser(SoupParser):
 
             try:
                 article_date = soup.find(class_='single-meta__date').get_text()
+                article_date= parser.parse(article_date, parserinfo=parserinfo, ignoretz=True)
                 article_tag = soup.find(class_='single__text')
-                p_list = article_tag.find_all('p', class_=False)
-                for p in p_list:
-                    for tag in p.find_all():
-                        if tag.name not in valid_tags:
-                            tag.decompose()
+                for tag in article_tag.find_all():
+                    if tag.name not in valid_tags:
+                        tag.decompose()
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
@@ -125,7 +123,7 @@ class SunoParser(SoupParser):
     news_source = 'Suno'
 
     def parse(self) -> None:
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -133,14 +131,12 @@ class SunoParser(SoupParser):
             soup = BeautifulSoup(page, "html.parser")
 
             try:
-                time_tag = soup.find('time')
-                article_date = time_tag['datetime']
+                article_date = soup.find('time')['datetime']
+                article_date= parser.parse(article_date, ignoretz=True)
                 article_tag = soup.find(class_='newsContent__article')
-                p_list = article_tag.find_all('p', class_=False)
-                for p in p_list:
-                    for tag in p.find_all():
-                        if tag.name not in valid_tags:
-                            tag.decompose()
+                for tag in article_tag.find_all():
+                    if tag.name not in valid_tags:
+                        tag.decompose()
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
@@ -156,7 +152,7 @@ class G1Parser(SoupParser):
     news_source = 'G1'
 
     def parse(self) -> None:
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -164,14 +160,15 @@ class G1Parser(SoupParser):
             soup = BeautifulSoup(page, "html.parser")
 
             try:
-                time_tag = soup.find('time')
-                article_date = time_tag['datetime']
+                article_date = soup.find('time')['datetime']
+                article_date= parser.parse(article_date, ignoretz=True)
                 article_tag = soup.find('article')
                 p_list = article_tag.find_all('p', class_='content-text__container')
                 for p in p_list:
                     for tag in p.find_all():
                         if tag.name not in valid_tags:
                             tag.decompose()
+                article_text = ' '.join(p.get_text(separator=' ', strip=True) for p in p_list)
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
@@ -180,14 +177,14 @@ class G1Parser(SoupParser):
             self._create_append_news_dict(url,
                                      soup.title.text,
                                      article_date,
-                                     article_tag.get_text(separator=' ', strip=True))
+                                     article_text)
 
 
 class UOLParser(SoupParser):
     news_source = 'UOL'
 
     def parse(self) -> None:
-        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'spam']
+        valid_tags = ['strong', 'b', 'em', 'i', 'p', 'a', 'span', 'ul', 'li']
 
         for url in self.url_list:
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -195,13 +192,12 @@ class UOLParser(SoupParser):
             soup = BeautifulSoup(page, "html.parser")
 
             try:
-                article_date = soup.find(p, class_='p-author time').get_text()
-                article_tag = soup.find('div', class_='text  ')
-                p_list = article_tag.find_all('p', class_=False)
-                for p in p_list:
-                    for tag in p.find_all():
-                        if tag.name not in valid_tags:
-                            tag.decompose()
+                article_date = soup.find('p', class_='time')['ia-date-publish']
+                article_date= parser.parse(article_date, ignoretz=True)
+                article_tag = soup.find('div', class_='text')
+                for tag in article_tag.find_all():
+                    if tag.name not in valid_tags:
+                        tag.decompose()
             except Exception as e:
                 print('ERRO NO PARSE DA URL ', url)
                 print(e)
